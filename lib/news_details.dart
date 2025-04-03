81f1655e2837334082cb287400e9dccbc290ea67
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-04-03 13:35:20 +0530

    Initial commit

--- lib/news_details.dart
+++ lib/news_details.dart
@@ -16,6 +16,7 @@ class NewsDeatils extends StatelessWidget {
           Image.network(
             newsArticle.urlToImage,
             height: 300,
+            fit: BoxFit.fill,
           )
         ],
       ),
